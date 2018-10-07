1be567b8123dc27461e829605c284dd133a3beb8
shounakmk219 <shounakmk219@gmail.com>
2018-10-07 22:37:59 +0530

    service package added

---
+++ WorkflowManager/src/main/java/com/workflow/service/GraphExtract.java
@@ -0,0 +1,11 @@
+package com.workflow.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GraphExtract {
+	private Map<String,String> config= new HashMap<String,String>();
+	private Map<String,String> input= new HashMap<String,String>();
+	private Map<String,String> output= new HashMap<String,String>();
+
+}
