3f81c1f5cd63cd08872aa57ed8d3b443b7d5eb33
Shubham tidke <30626268+Shubhamm165@users.noreply.github.com>
2018-10-08 14:04:11 +0530

    Update controllers.js

--- WorkflowManager/src/main/webapp/scripts/controllers.js
+++ WorkflowManager/src/main/webapp/scripts/controllers.js
@@ -21,7 +21,7 @@ app.controller('FormController', function($scope,$http, $location) {
     }
     )
   };
-  $scope.loadData("https://api.myjson.com/bins/lbzsc");
+  $scope.loadData("https://api.myjson.com/bins/70bwc");
 $scope.onSubmit = function(form) {
     // First we broadcast an event so all fields validate themselves
     $scope.$broadcast('schemaFormValidate');
